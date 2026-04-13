87557baa50624a5a9ae682571fb2c73c9ec1136a
nirasha <jayamannanirasha8@gmail.com>
2026-04-13 22:23:14 +0530

    Add the image in profile

--- src/component/profile.js
+++ src/component/profile.js
@@ -1,15 +1,18 @@
 function Profile() {
-    const user = {
+  const user = {
+    picture: "myPhoto.jpg",
     name: "Nirasha Jayamanna",
     email: "nirashajayamanna3@gmail.com",
     bio: "Software Engineering Student",  
   };
+
   return (
     <div className="profile-card">
-      
+      <img src={user.picture} alt="Profile" />
       <h2>{user.name}</h2>
-      <p>{user.email}</p>
       <p>{user.bio}</p>
+      <p>{user.email}</p>
+      
     </div>
   );
 }
